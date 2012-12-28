6ee3f547447626c377d24fba0a5ff011d081a70b
Earl St Sauver <estsauver@gmail.com>
2012-12-28 15:33:00 -0800

    Add Setup.py to directory

---
+++ neural_networks/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='senior_project',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/estsauver/bearded-ironman',
+    license='No License at this time',
+    author='Earl St Sauver',
+    author_email='estsauver@gmail.com',
+    description=''
+)
