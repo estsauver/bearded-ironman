6649665cc42a77a3abeec5db2fbac8dc928c1c77
Earl St Sauver <estsauver@gmail.com>
2012-12-31 13:05:43 -0800

    Add another network example and documentation

---
+++ neural_networks/network_documentation.markdown
@@ -0,0 +1,4 @@
+
+## Weighted Data Set
+There appears to be some weighted data set functionality already included in PyBrain. That's located here, in the
+SequentialDataSet class.
--- neural_networks/network_example1.py
+++ neural_networks/network_example1.py
@@ -1,6 +1,6 @@
 __author__ = 'estsauver'
 
-from pybrain.structure import RecurrentNetwork, LinearLayer, SigmoidLayer, FullConnection
+from pybrain import RecurrentNetwork, LinearLayer, SigmoidLayer, FullConnection
 
 # Here we define the labels for our inputs. The length of these is used
 # to calculate the size of the layers, so changing the number of these does impact the
@@ -23,10 +23,12 @@ net.addInputModule(inputLayer)
 
 # hiddenLayers is a list that holds all of the layers that are created in order.
 hiddenLayers=[]
-for i in range(len(hiddenLayerSizes)):
+
 #   Add a hidden layer for each hidden layer defined and give it a name.
+for i in range(len(hiddenLayerSizes)):
     hiddenLayers.append(SigmoidLayer(hiddenLayerSizes[i],name="HL Number {}".format(i+1)))
     net.addModule(hiddenLayers[i])
+#    Adds connections, either for the input to the first hidden or from hidden layer to hidden layer
     if i == 0:
         input_to_first_hidden = FullConnection(inputLayer,hiddenLayers[i])
         net.addConnection(input_to_first_hidden)
@@ -34,12 +36,17 @@ for i in range(len(hiddenLayerSizes)):
         hidden_to_hidden = FullConnection(hiddenLayers[i-1],hiddenLayers[i])
         net.addConnection(hidden_to_hidden)
 
+net.addRecurrentConnection(FullConnection(net["HL Number 1"],net["HL Number 1"], name="Recurrent Connection"))
+
 outputLayer = LinearLayer(len(netOutput), name="Output Layer")
 net.addOutputModule(outputLayer)
 hidden_to_output=FullConnection(hiddenLayers[-1],outputLayer)
 net.addConnection(hidden_to_output)
 net.sortModules()
 
-print net
+print net.activate([1,1,1,1])
+print net.activate([1,1,1,1])
+print net.activate([1,1,1,1])
+print net.activate([1,1,1,1])
 
 
