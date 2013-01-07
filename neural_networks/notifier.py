7c5da0f3b7c04e35d545132db54a6fbc6a04e8e6
Earl St Sauver <estsauver@gmail.com>
2013-01-07 14:08:29 -0800

    Add rough draft for error handler

--- neural_networks/notifier.py
+++ neural_networks/notifier.py
@@ -1,7 +1,52 @@
 __author__ = 'estsauver'
+import datetime
 from twilio.rest import TwilioRestClient
+
 account = "AC8c55c6e9f0c1dd3532c3302b36ff5179"
 token = "c8908533bb3ed7698cfac71832747657"
-client = TwilioRestClient(account,token)
+client = TwilioRestClient(account, token)
 
-message = client.sms.messages.create(to="+18572378675",body="The reactor is melting!", from_="+17694473275")
+class ErrorHandler(object):
+    def __init__(self, experiment):
+        self.badData = self.BadData()
+        self.experiment = experiment
+
+
+    def alert(self, bodyText="There's a problem with the reactor"):
+        message = client.sms.messages.create(to="+18572378675", body=bodyText, from_="+17694473275")
+        print message
+
+    def newError(self, type, time, values):
+        self.badData.flush()
+        self.BadData.add_bad_data(self.badData, type, time, values)
+        self.checkAlertConditions()
+
+
+    def checkAlertConditions(self):
+        #This calls all of the functions which separately determine the existence of a problem. Each of the functions
+        # can separetly process control scheme logic.
+        self.badDataCount()
+
+    def badDataCount(self):
+        if self.badData.count() > 5:
+            self.alert(bodyText="5 recent bad data messages")
+            return True
+
+    class BadData(object):
+        def __init__(self):
+            self.data = []
+            #The amount of time that we remember bad data for
+            self.badDataMemory = 1000
+
+        def flush(self):
+            while (datetime.datetime.now() - self.data[0][1]) > datetime.timedelta(seconds=self.badDataMemory):
+                self.data.pop()
+
+        def add_bad_data(self, type, time, values):
+            self.data.append((type, time, values))
+
+        def count(self):
+            return len(self.data)
+
+
+
