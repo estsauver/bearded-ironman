048c4a3ad61ef859bda8bb6a1c7fa59926e3b041
J. Abel <j.h.abel01@gmail.com>
2013-02-03 19:50:26 -0500

    Updated scale
    Scale based on the defaults from Bioflo3000 manual. - JA

--- neural_networks/sensorCalibration.py
+++ neural_networks/sensorCalibration.py
@@ -1,14 +1,14 @@
 __author__ = 'estsauver'
 
+#scale 0 --> 1 sensor input
 
 def temperature(pins):
     #We only need one pin to get the temp
     assert len(pins) == 1
-
     # If the arduino hasn't returned any data, send back a False value so it won't be processed.
     if pins[0] == None:
         return (False, -1)
-    value = pins[0] * 100
+    value = 100*pins[0]/(1024*5)
     error = 2
     return (value, error)
 
@@ -16,13 +16,29 @@ def temperature(pins):
 def pH(pins):
     ''' Takes a list of pin values and returns the value and the error.
     '''
-    assert len(pins) == 2
-    #If either pin has no
-    if pins[0] == None or pins[1] == None:
+    assert len(pins) == 1
+    if pins[0] == None:
         return (False, -1)
-    value = (pins[0] - pins[1]) * 1000
+    value = 2+10*pins[1]/(1024*5)
     error = 3
     return (value, error)
 
+def DO(pins):
+    ''' Takes a list of pin values and returns the value and the error.
+    '''
+    assert len(pins) == 1
+    if pins[0] == None:
+        return (False, -1)
+    value = 200*pins[2]/1024*5)
+    error = 4
 
-sensorPins = {temperature: [1], pH: [2, 3]}
+def Agitation(pins):
+    ''' Takes a list of pin values and returns the value and the error.
+    '''
+    assert len(pins) == 1
+    if pins[0] == None:
+        return (False, -1)
+    value = 1500*pins[3]/(1024*5)
+    error = 5
+    
+sensorPins = {temperature: [1], pH: [2], DO: [3], Agitataion: [4]}
